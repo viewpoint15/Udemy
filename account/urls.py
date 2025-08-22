aec77f803b001181da44b80fd7642a67ca030026
Pragadees T <pragadeesT2002@gmail.com>
2025-08-22 07:17:15 +0530

    this is my udemy course files

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,9 @@ from account import views
 
 urlpatterns = [
     path('',views.index,name = "index"),
-    path('second_page',views.second_page,name='second_page')
+    path('second_page',views.second_page,name='second_page'),
+    path('third_page',views.third_page,name='third_page'),
+    path('imagepage',views.imagepage,name='imagepage'),
+    path('imagepage2',views.imagepage2,name='imagepage2'),
+    path('imagepage3/<str:imagesname>',views.imagepage3,name='imagepage3')
 ]
--- account/views.py
+++ account/views.py
@@ -15,3 +15,37 @@ def index(request):
 
 def second_page(request):
     return render(request,'second_page.html')
+
+def third_page(request):
+    var = 'Hello Professor'
+    fruits = ["apple","grasps","mango"]
+    num1 , num2  = 3,4
+    ans = num1 > num2
+    memory = {
+        'var' : var,
+        'fruits': fruits,
+        'num1': num1,
+        'num2': num2,
+        'ans': ans
+    }
+    return render(request,'third_page.html',context = memory)
+
+def imagepage(request):
+    return render(request,'imagepage.html')
+
+def imagepage2(request):
+    return render(request,'imagepage2.html')
+
+def imagepage3(request,imagesname):
+    imagename = str(imagesname);
+    imagename = imagename.lower();
+    print(imagename)
+    if imagename == "Abstract ironman.jpg":
+        var = True
+    else:
+        imagename == "UltrA  MAX series.jpg" 
+        var = False
+    memory ={
+        'var': var
+    }   
+    return render(request,'imagepage3.html',context = {'var': var})
